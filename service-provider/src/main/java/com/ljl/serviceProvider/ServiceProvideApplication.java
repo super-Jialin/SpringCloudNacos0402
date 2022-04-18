4dbc5b7046cb1ccd6d94470c9be9e3655daa142a
Glenn <649692327@qq.com>
2022-04-18 08:08:47 +0800

    20220418

--- service-provider/src/main/java/com/ljl/serviceProvider/ServiceProvideApplication.java
+++ service-provider/src/main/java/com/ljl/serviceProvider/ServiceProvideApplication.java
@@ -1,21 +1,24 @@
-package com.ljl.service01;
+package com.ljl.serviceProvider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
-public class ServiceApplication01 {
+//@EnableDiscoveryClient
+@EnableSwagger2
+public class ServiceProvideApplication {
     public static void main(String[] args) {
-       ConfigurableApplicationContext applicationContext = SpringApplication.run(ServiceApplication01.class);
-       Environment env = applicationContext.getEnvironment();
-       String port = env.getProperty("server.port");
-       String path = env.getProperty("server.servlet.context-path");
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ServiceProvideApplication.class);
+
+        Environment env = applicationContext.getEnvironment();
+
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+
         System.out.println("\n--------------------------------------\n\t" +
                 "Application is running! Access URLs:\n\t" +
                 "Local: \t\thttp://localhost:" + port + path+ "/index.html\n\t" +
